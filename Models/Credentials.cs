40f290dcd07a6adbc5a5eb9b5a5ff2a9e170795c
kondaskondas <kondas.kondas@gmail.com>
2015-04-24 14:50:11 -0500

    User credentials

---
+++ Models/Credentials.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstsStepsRUI.Models
+{
+    public class Credential
+    {
+        public User User { get; set; }
+        public string Password { get; set; }
+
+        public Credential(User user, string password)
+        {
+            User = user;
+            Password = password;
+        }
+    }
+}
